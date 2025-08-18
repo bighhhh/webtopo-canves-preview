d64b7232e7fb1eec92ff993d6fabbfa033861cde
bighhhh <bighhhh@users.noreply.github.com>
2025-08-18 07:30:31 +0000

    deploy: bighhhh/webtopo-canves@6c4b510f54ff3ffdd69070fb578d99b5cf504279

---
+++ assets/scriptWorker.browser-BgYqWMem-DOOnLx2A.js
@@ -0,0 +1,2 @@
+function r(e){return new Worker("/assets/scriptWorker.browser-BcOpVmi0.js",{name:e==null?void 0:e.name})}export{r as default};
+//# sourceMappingURL=scriptWorker.browser-BgYqWMem-DOOnLx2A.js.map
